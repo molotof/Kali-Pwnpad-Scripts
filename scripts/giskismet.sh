ebb78ea2148e48d1468996ad4207063616325dee
Binky Bear <binkybear@local>
2013-08-03 22:00:44 -1000

    Added all scripts
    Mix of personal, pwnpad, and pwnpadpak scripts for the Kali pwnpad.

--- scripts/giskismet.sh
+++ scripts/giskismet.sh
@@ -3,11 +3,13 @@
 ## date format ##
 NOW=$(date +"%m-%d-%y-%H%M")
 
+echo "Adding Kismet data to local database"
 # add kismet data to giskmet database
 giskismet -x /opt/pwnpad/captures/kismet/Kismet*.netxml
 
 # export kml of all wireless data
 giskismet -q "select * from wireless" -o "/opt/pwnpad/captures/kismet/kismet-$NOW.kml"
+echo "Created Kismet KML file for Google Earth"
 
 cd /sdcard/
 zip -rj "kismet-captures-$NOW.zip" /opt/pwnpad/captures/kismet/
@@ -15,4 +17,4 @@ echo "Successfully copied to /sdcard/kismet-captures-$NOW.zip"
 
 echo "Removing capture files..."
 
-rm /opt/pwnpad/captures/kismet/*
+wipe -f -i -r /opt/pwnpad/captures/kismet/*
