6b409690ef9178efe8f775c368c343eb4c1792c9
Binky Bear <binkybear@local>
2013-08-30 22:49:03 -1000

    added for each file
    Will now process all netxml files in a folder and treat each one by
    itself.  I'm drunk and it works.

--- scripts/giskismet.sh
+++ scripts/giskismet.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
 ## date format ##
-NOW=$(date +"%m-%d-%y-%H%M")
+NOW=$(date +"%m-%d-%y-%H%M%S")
 
+# create database folder or check if one exsists
 mkdir -p "/opt/pwnpad/captures/kismet_db"
-giskismet -x /opt/pwnpad/captures/kismet/*.netxml --database "/opt/pwnpad/captures/kismet_db/wireless.dbl"
 
-# export kml of all wireless data
-giskismet -q "select * from wireless" -o "/opt/pwnpad/captures/kismet/kismet-$NOW.kml"
+# process all netxml files
+
+cd /opt/pwnpad/captures/kismet/
+for capture in $(find . -iname '*.netxml'); do
+echo "Adding $capture to datbase"
+sleep 5
+giskismet -x "$capture" --database "/opt/pwnpad/captures/kismet_db/wireless.dbl"
+
+done
+
+# export kml of all wireless data and copy files to sdcard
+
+giskismet --database "/opt/pwnpad/captures/kismet_db/wireless.dbl" -q "select * from wireless" -o "/opt/pwnpad/captures/kismet/kismet-$NOW.kml"
 echo "Created kismet-$NOW.kml file for Google Earth"
 cd /sdcard/
 zip -rj "kismet-captures-$NOW.zip" /opt/pwnpad/captures/kismet/
@@ -15,11 +26,11 @@ echo "Successfully copied to /sdcard/kismet-captures-$NOW.zip"
 
 # option to erase files
 
-read -p "Would you like to erase files in Kismet folder? (y/n)" CONT
+read -p "Would you like to erase all files in Kismet folder? (y/n)" CONT
 if [ "$CONT" == "y" ]; then
 	echo "Removing capture files..."
 	wipe -f -i -r /opt/pwnpad/captures/kismet/*
 else
-  echo "Success!";
+  echo "All files copied successfully!";
 fi
 exit
