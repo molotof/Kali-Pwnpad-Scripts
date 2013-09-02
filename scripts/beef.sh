50fc9629cec321aa3bbe4e2f44de7f377177d257
Binky Bear <binkybear@local>
2013-09-02 07:45:59 -1000

    fixed beef start folder

--- scripts/beef.sh
+++ scripts/beef.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
-# Credit goes to TH3CR@CK3R for making this script at http://top-hat-sec.com/forum/index.php?topic=3127.0
 
 echo ""
-echo "BEEF USES PORT 3000 AS DEFAULT"
+echo "BeEF uses port 3000 as a default with login/password: beef"
 echo "PLEASE SELECT AN OPTION TO CONTINUE"
 echo ""
-echo "1 = USE DEFAULT [3000]"
-echo "2 = SELECT A DIFFERENT PORT"
+echo "1 = Use Default [3000]"
+echo "2 = Select a different port"
 echo ""
 echo "OPTION NUMBER: \c"
 read option
 
-
 if [ $option = "1" ]; then
-
-echo "STARTING BEEF SERVER"
-xterm -e sudo beef-server &
+	echo "STARTING BEEF SERVER"
+	cd /usr/share/beef-xss/
+  ./beef
 exit
 
 else
@@ -28,7 +26,8 @@ if [ $option = "2" ]; then
   sleep 1
   echo ""
   echo "STARTING BEEF SERVER"
-  xterm -e sudo beef-server -p $port &
+  cd /usr/share/beef-xss/
+  ./beef -p $port
   exit
 
 fi
