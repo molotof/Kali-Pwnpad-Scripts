cd11cc9b3f4fa41703a6db1a6df102911d1fe3ee
Binky Bear <binkybear@local>
2013-10-12 20:15:20 -1000

    Added elementalx kernel load support

--- userinit.sh
+++ userinit.sh
@@ -1,4 +1,7 @@
 su
+echo 1 > /sys/android_touch/doubletap2wake
+echo 1 > /sys/android_touch/shortsweep
+echo 2 > /sys/android_touch/sweep2wake
 echo "Sleep for 20 seconds to allow mounting"
 sleep 20
 mount -o remount,rw -t yaffs2 /dev/block/mtdblock3 /system
@@ -29,13 +32,17 @@ sysctl -w kernel.shmmax=268435456
 sysctl -w net.ipv4.ip_forward=1
 echo "nameserver 8.8.8.8" > $mnt/etc/resolv.conf
 echo "nameserver 8.8.4.4" >> $mnt/etc/resolv.conf
+echo "options single-request-reopen" >> resolv.conf
 echo "127.0.0.1 localhost" > $mnt/etc/hosts
 
 # execute startup script
 echo "Starting SSH server..."
 chroot $mnt /etc/init.d/ssh start
-# echo "Starting VNC server..."
-chroot $mnt /etc/init.d/vncboot start
-# /usr/bin/vncserver :0 -geometry 1200x800
 
-echo "Kali SSH/VNC now running in background..."
+# Just testing things...
+# echo "Starting VNC server..."
+# chroot $mnt /etc/init.d/vncboot start
+# /usr/bin/vncserver :0 -geometry 1200x800
+#insmod /data/local/dhd.ko iface_name=wlan0 firmware_path=/data/local/bcm4330_sta.bcmon.bin nvram_path=/system/etc/nvram.txt
+#ifconfig wlan0 up
+# modprobe bcmdhd
