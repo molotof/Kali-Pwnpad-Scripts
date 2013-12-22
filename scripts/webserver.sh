934f757807fc151023c99b76d13982938a1ec5be
Binky Bear <binkybear@local>
2013-12-22 09:14:39 -1000

    add autossh

--- scripts/webserver.sh
+++ scripts/webserver.sh
@@ -4,7 +4,7 @@ if [ -d "/opt/pwnpad/web-interface" ]; then
 	cd /opt/pwnpad/web-interface
 	php -S localhost:8000
 else
-	apt-get -y install php5 php5-cli php-pear
+	apt-get -y install php5 php5-cli php-pear autossh
 	cd /opt/pwnpad
 	git clone https://github.com/binkybear/web-interface.git
 	echo "Starting webserver on localhost:8000"
