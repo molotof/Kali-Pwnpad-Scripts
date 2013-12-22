b19596494b304646bfe0b71f54459d91cc0224be
Binky Bear <binkybear@local>
2013-12-22 07:46:38 -1000

    fix start folder

--- scripts/webserver.sh
+++ scripts/webserver.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ -d "/opt/pwnpad/web-interface" ]; then
 	echo "Starting webserver on localhost:8000"
+	cd /opt/pwnpad/web-interface
 	php -S localhost:8000
 else
 	apt-get -y install php5 php5-cli php-pear
@@ -8,5 +9,4 @@ else
 	git clone https://github.com/binkybear/web-interface.git
 	echo "Starting webserver on localhost:8000"
 	php -S localhost:8000
-
 fi
