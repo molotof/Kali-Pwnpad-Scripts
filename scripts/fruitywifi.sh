2b09c5096899587c93861b3bc2dab9a1ecfbab01
Binky Bear <binkybear@local>
2013-12-25 11:38:14 -1000

    first commit of fruitywifi test

---
+++ scripts/fruitywifi.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ -d "/var/www/FruityWifi" ]; then
+	echo "Starting webserver on localhost:8000"
+	service atd start
+	cd /var/www/FruityWifi/
+	php -S localhost:8000
+else
+	echo "This will install necessary pre-requistes such as"
+	echo "autossh, PHP, and AT needed for webserver"
+	cd /opt/pwnpad
+	git clone https://github.com/binkybear/FruityWifi.git
+	cd FruityWifi
+	chmod +x install-kalipwn.such
+	./install-kalipwn.sh;
+	echo "Starting webserver on localhost:8000"
+	cd /var/www/FruityWifi/
+	php -S localhost:8000
+fi
