88550c3e662787f95b37a16e1a03411ce18c8109
Binky Bear <binkybear@local>
2013-12-24 21:05:12 -1000

    added "at" which fixes a lot of pineapple stuff

--- scripts/webserver.sh
+++ scripts/webserver.sh
@@ -4,7 +4,9 @@ if [ -d "/opt/pwnpad/web-interface" ]; then
 	cd /opt/pwnpad/web-interface
 	php -S localhost:8000
 else
-	apt-get -y install php5 php5-cli php-pear autossh
+	echo "This will install necessary pre-requistes such as"
+	echo "autossh, PHP, and AT needed for webserver"
+	apt-get -y install php5 php5-cli php-pear autossh at
 	cd /opt/pwnpad
 	git clone https://github.com/binkybear/web-interface.git
 	echo "Starting webserver on localhost:8000"
