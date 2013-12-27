35a8a98cee2880341c4bcfc9ce72fa939ffb81c0
Binky Bear <binkybear@local>
2013-12-26 15:39:14 -1000

    clear and echo default login/password

--- scripts/fruitywifi.sh
+++ scripts/fruitywifi.sh
@@ -3,6 +3,8 @@ if [ -d "/var/www/FruityWifi" ]; then
 	echo "Starting webserver on localhost:8000"
 	service atd start
 	cd /var/www/FruityWifi/
+	clear
+	echo "Default login/password: admin"
 	php -S localhost:8000
 else
 	echo "This will install necessary pre-requistes such as"
@@ -12,6 +14,8 @@ else
 	cd FruityWifi
 	chmod +x install-kalipwn.sh
 	./install-kalipwn.sh;
+	clear
+	echo "Install finished. Default login/password: admin"
 	echo "Starting webserver on localhost:8000"
 	cd /var/www/FruityWifi/
 	php -S localhost:8000
